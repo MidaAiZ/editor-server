88b0a28118482973c2526e6df8629147971e1ed2
lihaoyu <825091124@qq.com>
2019-09-15 20:05:38 +0800

    generator

---
+++ src/main/java/com/mida/chromeext/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.mida.chromeext.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lihaoyu
+ * @date 2019/9/15 16:19
+ */
+@Service
+public class LoginService {
+
+    @Autowired
+    AdministratorService administratorService;
+
+    
+
+}
