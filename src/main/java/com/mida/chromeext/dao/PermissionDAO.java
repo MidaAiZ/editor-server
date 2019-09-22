bce30a5ce8f0b3e6fe07a40ab35f8a8163d4f9b1
lihaoyu <825091124@qq.com>
2019-09-22 12:50:21 +0800

    generate po and dao

---
+++ src/main/java/com/mida/chromeext/dao/PermissionDAO.java
@@ -0,0 +1,11 @@
+package com.mida.chromeext.dao;
+
+import com.mida.chromeext.mapper.PermissionMapper;
+
+/**
+ * @author lihaoyu
+ * @date 2019/9/22 12:47
+ */
+public interface PermissionDAO extends PermissionMapper {
+
+}
---
+++ src/main/java/com/mida/chromeext/dao/RolePermissonDAO.java
@@ -0,0 +1,11 @@
+package com.mida.chromeext.dao;
+
+import com.mida.chromeext.mapper.RolePermissionMapper;
+
+/**
+ * @author lihaoyu
+ * @date 2019/9/22 12:48
+ */
+public interface RolePermissonDAO extends RolePermissionMapper {
+
+}
