eb1390b478775e3251f5b341a559f7759f30da0b
lihaoyu <825091124@qq.com>
2019-09-24 20:53:41 +0800

    site search

--- src/main/java/com/mida/chromeext/controller/SiteController.java
+++ src/main/java/com/mida/chromeext/controller/SiteController.java
@@ -2,18 +2,27 @@ package com.mida.chromeext.controller;
 
 import java.util.List;
 
+<<<<<<< HEAD
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+=======
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+>>>>>>> site search
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mida.chromeext.pojo.Site;
 import com.mida.chromeext.service.SiteService;
+<<<<<<< HEAD
 import com.mida.chromeext.vo.SiteListQueryVo;
+=======
+import com.mida.chromeext.vo.SiteQueryVo;
+>>>>>>> site search
 
 /**
  * @author lihaoyu
