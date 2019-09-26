d9c4ef969d00a4a4fd1b333221cc34dce1d3282d
lihaoyu <825091124@qq.com>
2019-09-26 20:21:48 +0800

    fix conflict

--- src/main/java/com/mida/chromeext/controller/UsersController.java
+++ src/main/java/com/mida/chromeext/controller/UsersController.java
@@ -1,19 +1,22 @@
 package com.mida.chromeext.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.mida.chromeext.exception.BaseException;
 import com.mida.chromeext.pojo.User;
 import com.mida.chromeext.service.UserService;
 import com.mida.chromeext.utils.Result;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
-
 
 @RestController
 @RequestMapping("users")
