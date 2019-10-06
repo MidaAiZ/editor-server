b275c40836f182378d317b32877e52dcc8865a24
SinnerAiZ <1036570699@qq.com>
2019-10-06 23:57:04 +0800

    feat:添加用户登录状态获取接口

--- src/main/java/com/mida/chromeext/controller/UsersController.java
+++ src/main/java/com/mida/chromeext/controller/UsersController.java
@@ -1,8 +1,12 @@
 package com.mida.chromeext.controller;
 
+import com.mida.chromeext.annotation.CurrentUser;
+import com.mida.chromeext.annotation.LoginRequired;
+import com.mida.chromeext.utils.ResultCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +21,6 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import springfox.documentation.annotations.ApiIgnore;
-
 
 @RestController
 @RequestMapping("users")
@@ -44,5 +47,12 @@ public class UsersController {
             return Result.error(e.getCode(), e.getMessage());
         }
         return Result.ok(user);
+    }
+
+    @LoginRequired
+    @GetMapping("profile")
+    @ApiOperation("获取当前已登录用户信息，可以判断用户是否已登录")
+    public Result<User> profile(@ApiIgnore @CurrentUser User user) {
+        return user == null ? Result.error(ResultCode.NOT_LOGIN.code(), ResultCode.NOT_LOGIN.code()) : Result.ok(user);
     }
 }
