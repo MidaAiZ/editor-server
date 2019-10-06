5d79a6c54e7f5340f36f33ee7fb633ec306df689
SinnerAiZ <1036570699@qq.com>
2019-10-06 23:40:11 +0800

    feat:优化本地化的设置

--- src/main/java/com/mida/chromeext/resolver/MyLocaleResolver.java
+++ src/main/java/com/mida/chromeext/resolver/MyLocaleResolver.java
@@ -1,46 +1,38 @@
 package com.mida.chromeext.resolver;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.LocaleResolver;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
 /**
- * 自定义国际化语言解析器
- *
+ * @Description 自定义国际化处理器LocaleResolver
+ * SpringBoot为我们自动配置的LocalResolver，是根据请求的请求头中的"Accept-Language"获取判断的
+ * 现在改写成先从url路径上寻找是否有国际化语言变量，如果没有再从请求头的"Accept-Language"中获取
  */
 public class MyLocaleResolver implements LocaleResolver {
-
-    private static final String I18N_LANGUAGE = "i18n_language";
-    private static final String I18N_LANGUAGE_SESSION = "i18n_language_session";
-
+    /**
+     * 处理逻辑
+     * 接口核心方法，获取Locale，并返回
+     * @param request 请求
+     * @return Locale区域信息
+     */
     @Override
-    public Locale resolveLocale(HttpServletRequest req) {
-        String i18n_language = req.getParameter(I18N_LANGUAGE);
-        Locale locale = Locale.getDefault();
-        if(i18n_language != null) {
-            String[] language = i18n_language.split("_");
-            locale = new Locale(language[0], language[1]);
-
-            //将国际化语言保存到session
-            HttpSession session = req.getSession();
-            session.setAttribute(I18N_LANGUAGE_SESSION, locale);
-        }else {
-            //如果没有带国际化参数，则判断session有没有保存，有保存，则使用保存的，也就是之前设置的，避免之后的请求不带国际化参数造成语言显示不对
-            HttpSession session = req.getSession();
-            Locale localeInSession = (Locale) session.getAttribute(I18N_LANGUAGE_SESSION);
-            if(localeInSession != null) {
-                locale = localeInSession;
-            }
+    public Locale resolveLocale(HttpServletRequest request) {
+        String l = request.getParameter("lang");
+        if (StringUtils.isEmpty(l)) {
+            //路径上没有国际化语言参数，采用默认的（从请求头中获取）
+            return request.getLocale();
+        } else {
+            String[] split = l.split("_");
+            //语言、国家构造器
+            return split.length == 2 ? new Locale(split[0], split[1]) : new Locale(split[0], "");
         }
-        return locale;
     }
 
     @Override
-    public void setLocale(HttpServletRequest req, HttpServletResponse res, Locale locale) {
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
 
     }
-
 }
