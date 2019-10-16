edfef1e5ec103cc6ea11b70b6f80bdc5143d633f
SinnerAiZ <1036570699@qq.com>
2019-10-17 00:55:43 +0800

    feat:添加后台壁纸操作接口

---
+++ src/main/java/com/mida/chromeext/modules/controller/manage/BgPicturesController.java
@@ -0,0 +1,64 @@
+package com.mida.chromeext.modules.controller.manage;
+
+import com.mida.chromeext.annotation.CurrentAdmin;
+import com.mida.chromeext.components.shiro.PermisConstant;
+import com.mida.chromeext.modules.pojo.Admin;
+import com.mida.chromeext.modules.pojo.BgPicture;
+import com.mida.chromeext.modules.service.BgPictureService;
+import com.mida.chromeext.modules.vo.ListQueryVo;
+import com.mida.chromeext.utils.Result;
+import com.mida.chromeext.utils.ResultCode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+@RestController("mngBgPicturesController")
+@RequestMapping("manage/bg_pictures")
+@Api(value = "后台壁纸图片操作接口", tags = "{}")
+public class BgPicturesController {
+    @Autowired
+    private BgPictureService bgPictureService;
+
+    @GetMapping("")
+    @ApiOperation("获取系统中的壁纸列表")
+    @RequiresPermissions(PermisConstant.SHOW_BG_PICTURE)
+    public Result<List<BgPicture>> list(@RequestParam Integer pageNum, @RequestParam Integer pageSize) {
+        return Result.ok(bgPictureService.getList(new ListQueryVo(pageNum, pageSize)));
+    }
+
+    @GetMapping("{pid}")
+    @ApiOperation("获取主键获取一条记录")
+    @RequiresPermissions(PermisConstant.SHOW_BG_PICTURE)
+    public Result<BgPicture> show(@PathVariable Integer pid) {
+        BgPicture bgPicture = bgPictureService.getById(pid);
+        return bgPicture != null ? Result.ok(bgPicture) : Result.error(ResultCode.NOT_FOUND.code(), "No such picture with ID = " + pid.toString());
+    }
+
+    @PostMapping("")
+    @ApiOperation("添加一张壁纸")
+    @RequiresPermissions(PermisConstant.ADD_BG_PICTURE)
+    public Result<BgPicture> create(@RequestBody @Validated BgPicture bgPicture, @ApiIgnore @CurrentAdmin Admin admin) {
+        return bgPictureService.create(admin.getAid(), bgPicture) ? Result.ok(bgPicture) : Result.error();
+    }
+
+    @PutMapping("{pid}")
+    @ApiOperation("通过主键更新一条数据")
+    @RequiresPermissions(PermisConstant.MODIFY_BG_PICTURE)
+    public Result<Boolean> update(@PathVariable Integer pid, @RequestBody BgPicture bgPicture) {
+        bgPicture.setPid(pid);
+        return bgPictureService.updateById(bgPicture) ? Result.ok(true) : Result.error();
+    }
+
+    @DeleteMapping("{pid}")
+    @ApiOperation("通过主键删除一条数据")
+    @RequiresPermissions(PermisConstant.DELETE_BG_PICTURE)
+    public Result<Boolean> delete(@PathVariable Integer pid) {
+        return bgPictureService.deleteById(pid) ? Result.ok(true) : Result.error();
+    }
+}
--- src/main/java/com/mida/chromeext/modules/service/BgPictureService.java
+++ src/main/java/com/mida/chromeext/modules/service/BgPictureService.java
@@ -1,11 +1,17 @@
 package com.mida.chromeext.modules.service;
 
+import com.github.pagehelper.PageHelper;
 import com.mida.chromeext.modules.dao.BgPictureDAO;
 import com.mida.chromeext.modules.pojo.BgPicture;
+import com.mida.chromeext.modules.pojo.BgPictureExample;
+import com.mida.chromeext.modules.vo.ListQueryVo;
 import com.mida.chromeext.utils.NumConst;
+import io.swagger.models.auth.In;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,9 +24,60 @@ public class BgPictureService {
     @Autowired
     BgPictureDAO bgPictureDAO;
 
+    /**
+     * 随机获取一条数据
+     * @return
+     */
     public BgPicture getRandomBgPicture() {
         List<BgPicture> list = bgPictureDAO.listRandomBgPicture(NumConst.NUM1);
         return list.get(NumConst.NUM0);
     }
 
+    /**
+     * 通过主键获取一条记录
+     */
+    public BgPicture getById(Integer pid) {
+        return bgPictureDAO.selectByPrimaryKey(pid);
+    }
+
+    /**
+     * 获取多条数据
+     * @param queryVo
+     * @return
+     */
+    public List<BgPicture> getList(ListQueryVo queryVo) {
+        BgPictureExample ex = new BgPictureExample();
+        PageHelper.startPage(queryVo);
+        return bgPictureDAO.selectByExample(ex);
+    }
+
+    /**
+     * 插入一条数据
+     * @param picture
+     * @return
+     */
+    public Boolean create(Integer adminId, BgPicture picture) {
+        Date date = new Date();
+        picture.setCreatedAt(date);
+        picture.setCreatedBy(adminId);
+        return bgPictureDAO.insertSelective(picture) > 0;
+    }
+
+    /**
+     * 通过主键更新一条数据
+     * @param picture
+     * @return
+     */
+    public Boolean updateById(BgPicture picture) {
+        return bgPictureDAO.updateByPrimaryKeySelective(picture) > 0;
+    }
+
+    /**
+     * 通过主键删除一条记录
+     * @param pid
+     * @return
+     */
+    public Boolean deleteById(Integer pid) {
+        return  bgPictureDAO.deleteByPrimaryKey(pid) > 0;
+    }
 }
