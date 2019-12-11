335480ab0f93311e2448383116731b4c7993dde9
SinnerAiZ <1036570699@qq.com>
2019-12-11 19:25:57 +0800

    feat:优化log配置

--- src/main/java/net/tabplus/api/aop/WebLogAop.java
+++ src/main/java/net/tabplus/api/aop/WebLogAop.java
@@ -1,6 +1,7 @@
 package net.tabplus.api.aop;
 
 import com.alibaba.fastjson.JSONObject;
+import com.sun.xml.internal.ws.client.ResponseContext;
 import lombok.extern.slf4j.Slf4j;
 import net.tabplus.api.utils.Result;
 import org.aspectj.lang.JoinPoint;
@@ -9,8 +10,13 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
 
 /**
  * @author www.exception.site (exception 教程网)
@@ -36,6 +42,25 @@ public class WebLogAop {
         // 开始打印请求日志
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
+//        attributes.getResponse()
+        Object[] args = joinPoint.getArgs();
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest || args[i] instanceof ServletResponse || args[i] instanceof MultipartFile) {
+                //ServletRequest不能序列化，从入参里排除，否则报异常：java.lang.IllegalStateException: It is illegal to call this method if the current request is not in asynchronous mode (i.e. isAsyncStarted() returns false)
+                //ServletResponse不能序列化 从入参里排除，否则报异常：java.lang.IllegalStateException: getOutputStream() has already been called for this response
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        String paramter = "";
+        if (arguments != null) {
+            try {
+                paramter = JSONObject.toJSONString(arguments);
+            } catch (Exception e) {
+                paramter = arguments.toString();
+            }
+        }
 
         // 打印请求相关参数
         log.info("========================================== Start ==========================================");
@@ -49,7 +74,7 @@ public class WebLogAop {
         // 打印请求的 IP
         log.info("IP             : {}", request.getRemoteAddr());
         // 打印请求入参
-        log.info("Request Args   : {}", JSONObject.toJSONString(joinPoint.getArgs()));
+        log.info("Request Args   : {}", arguments);
     }
 
     /**
@@ -73,8 +98,11 @@ public class WebLogAop {
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         long startTime = System.currentTimeMillis();
         Result result = (Result) proceedingJoinPoint.proceed();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletResponse response = attributes.getResponse();
+
         // 打印出参
-        log.info("Response       : {} {}", result.getCode(), result.getMessage());
+        log.info("Response       : Status {}, Code: {}, Message: {}, Size: {}KB", response.getStatus(), result.getCode(), result.getMessage(), response.getBufferSize() / 1024);
         // 执行耗时
         log.info("Time-Consuming : {} ms", System.currentTimeMillis() - startTime);
         return result;
